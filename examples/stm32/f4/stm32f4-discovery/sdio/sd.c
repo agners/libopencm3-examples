bb4720ce1f2efcf73e39ffd509b88b89dfb2b15e
Stefan Agner <stefan@agner.ch>
2015-02-16 23:36:02 +0100

    sdio: wait until command is sent, return the last STA value

--- examples/stm32/f4/stm32f4-discovery/sdio/sd.c
+++ examples/stm32/f4/stm32f4-discovery/sdio/sd.c
@@ -4,26 +4,33 @@
 
 uint32_t sd_command(uint32_t cmd, uint32_t resp, uint32_t arg)
 {
+	uint32_t sta;
+
 	SDIO_ICR = (SDIO_STA_CCRCFAIL | SDIO_STA_CTIMEOUT | SDIO_STA_CMDREND | SDIO_STA_CMDSENT);
 
 	SDIO_ARG = arg;
 
 	SDIO_CMD = (cmd & SDIO_CMD_CMDINDEX_MSK) | (resp & SDIO_CMD_WAITRESP_LONG) | SDIO_CMD_CPSMEN;
 
-	if (resp==0x00) {
-		while(!(SDIO_STA & (SDIO_STA_CTIMEOUT | SDIO_STA_CMDSENT))) {};
-	}
-	else
-	{
-		while (!(SDIO_STA & (SDIO_STA_CTIMEOUT | SDIO_STA_CMDREND | SDIO_STA_CCRCFAIL))) {};
+	while (true) {
+		sta = SDIO_STA;
+
+		/* Wait until command is done */
+		if (sta & SDIO_STA_CMDACT)
+			continue;
+
+		if (resp && sta & (SDIO_STA_CTIMEOUT | SDIO_STA_CMDREND | SDIO_STA_CCRCFAIL))
+			break;
+		else if (!resp && sta & (SDIO_STA_CTIMEOUT | SDIO_STA_CMDSENT))
+			break;
 	}
 
-	return SDIO_STA;
+	return sta;
 }
 
 uint32_t sd_response(uint32_t *response, uint32_t type)
 {
-	if(type == RESP_R1 || type == RESP_R1b)
+	if (type == RESP_R1 || type == RESP_R1b)
 	{
 		*response=SDIO_RESP1;
 
@@ -33,6 +40,5 @@ uint32_t sd_response(uint32_t *response, uint32_t type)
 		}*/
 	}
 
+	return 0;
 }
-
-
